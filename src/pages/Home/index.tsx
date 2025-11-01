753fbc469981bd1f1c074c6f16774db1c24fbb6f
BrunoYamamoto2204 <brunoeiky2006@gmail.com>
2025-11-01 13:59:21 -0300

    Home: Quick Status

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "../../components/Container";
 import { MainTemplate } from "../../templates/MainTemplate";
 
@@ -7,6 +7,7 @@ import { Title } from "../../components/Title";
 import GeralDatePicker from "../../components/GeralDatePicker";
 import { CircleCheckIcon, ClipboardListIcon, HourglassIcon } from "lucide-react";
 import { formatBrDate } from "../../utils/format-date";
+import { getOrders, type Order } from "../../services/ordersApi";
 
 
 export function Home() {
@@ -15,10 +16,39 @@ export function Home() {
         return date.toLocaleDateString('sv-SE');
     }
 
+    const [ orders, setOrders ] = useState<Order[]>([])
+
+    useEffect(() => {
+        const loadOrders = async () => {
+            try{
+                const orderData = await getOrders()
+                setOrders(orderData)
+            } catch (error) {
+                console.log("[500] - Erro ao carregar produtos: ", error)
+            } 
+        }
+
+        loadOrders()
+    }, [])
+
+    const countPendingOrders = () => {
+        let countPending = 0;
+        let concluedCounter = 0;
+
+        orders.map(order => (
+            order.status === "Pendente" ? countPending++ : concluedCounter++
+        ))
+
+        return {countPending, concluedCounter}
+    }
+
+    const { countPending, concluedCounter } = countPendingOrders()
+
     // String yyyy-MM-dd
-    const [date, setDate] = useState(formatDateString(new Date())) 
+    const [ date, setDate ] = useState(formatDateString(new Date())) 
     
     return (
+        
         <MainTemplate>
             <Container>
                 <div className={styles.HomeContainer}>
@@ -71,7 +101,7 @@ export function Home() {
                             </div>
                             <div className={styles.statsContent}>
                                 <h3>Pedidos Pendentes</h3>
-                                <p>4</p>
+                                <p>{countPending}</p>
                             </div>
                         </div>
                         <div className={styles.quickStatsBox}>
@@ -80,7 +110,7 @@ export function Home() {
                             </div>
                             <div className={styles.statsContent}>
                                 <h3>Pedidos Concluídos</h3>
-                                <p>16</p>
+                                <p>{concluedCounter}</p>
                             </div>
                         </div>
                         <div className={styles.quickStatsBox}>
@@ -89,7 +119,7 @@ export function Home() {
                             </div>
                             <div className={styles.statsContent}>
                                 <h3>Pedidos Totais</h3>
-                                <p>20</p>
+                                <p>{orders.length}</p>
                             </div>
                         </div>
                     </div>

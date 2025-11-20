577871d89f0b2061712c819893bb240e1b374ee2
BrunoYamamoto2204 <brunoeiky2006@gmail.com>
2025-11-20 19:23:12 -0300

    Fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URL || "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+})
